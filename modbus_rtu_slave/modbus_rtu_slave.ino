a1aba3e9a13af3f64de308b62d687c0e3cf804da
pmwesigw <pmwesigw@vilant.com>
2024-06-22 17:51:24 +0300

    Added code to read analog value

--- modbus_rtu_slave/modbus_rtu_slave.ino
+++ modbus_rtu_slave/modbus_rtu_slave.ino
@@ -6,23 +6,21 @@ const uint8_t slaveID = 1;
 const uint32_t baud = 9600;
 
 uint16_t holdingRegisters[20] = {0};
-uint16_t value = 0;
+uint16_t sensor = 0;
 
 void setup() {
-  // put your setup code here, to run once:
+  // configure Holding Registers
   modbus_rtu_slave.configureHoldingRegisters(holdingRegisters, 20);
-  //modbus.configureDiscreteInputs(discreteInputs, 2);
+  // Start slave
   modbus_rtu_slave.begin(slaveID, baud, SERIAL_8N1);
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  value = value + 3;
-  holdingRegisters[1] = value;
-  holdingRegisters[2] = 3;
-  modbus_rtu_slave.poll();
-  //Serial.println(value);
-
-  //delay(5000);
+  // Read Analog value
+  sensor = analogRead(A1);
+  // Write value to Holding register
+  holdingRegisters[0] = sensor;
+  // Modbus Poll
+  modbus_slave.poll();
 }
